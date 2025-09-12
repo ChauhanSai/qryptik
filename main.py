d2a80f888430abf2f82c8e32ad2febf4c60a3400
rubyTanuki <aebrown808@gmail.com>
2025-09-12 12:44:24 -0500

    init project files and .venv

---
+++ main.py
@@ -0,0 +1,11 @@
+def KeyGen(n, k, t, r) -> (str, str): #returns (public key, private key)
+    return ("","");
+
+def encrypt(public_key, message) -> str: # returns ciphertext
+    return "";
+
+def decrypt(private_key, ciphertext) -> str: # returns message
+    return "";
+
+
+def main():
